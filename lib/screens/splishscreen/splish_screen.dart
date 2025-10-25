b19bf9d6eb27f0f105d41acfa84b2f758f84b8d7
Azhar Iqbal <niazi7589@gmail.com>
2025-10-25 12:50:33 +0500

    changed

--- lib/screens/splishscreen/splish_screen.dart
+++ lib/screens/splishscreen/splish_screen.dart
@@ -13,7 +13,7 @@ class _SplishScreenState extends State<SplishScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const Walkthrough()),
